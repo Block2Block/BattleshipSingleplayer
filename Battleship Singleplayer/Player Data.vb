4bb0e4699685293c5e9e79ed8aa0954944dbeafc
Blocky McBlockins <thebarkerkids@hotmail.com>
2019-02-06 20:49:02 +0000

    Iteration 1 Complete - Login and Accounts Management

---
+++ Battleship Singleplayer/Player Data.vb
@@ -0,0 +1,11 @@
+﻿Module Player_Data
+    'User Login Details
+    Public playerUserID As Integer = 0
+    Public playerUsername As String
+
+    'User Player Data
+    Public playerXP As Int64 = 0
+    Public playerLevel As Byte
+    Public playerWins As Int16 = 0
+    Public playerGamesPlayed As Int16 = 0
+End Module
